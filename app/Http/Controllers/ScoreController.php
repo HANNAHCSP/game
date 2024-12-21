4364f51df75b4d2273c06c5c43fab58468b6c08b
HANNAHCSP <133366701+HANNAHCSP@users.noreply.github.com>
2024-12-21 23:00:01 +0200

    nothing

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Score;
-use App\Models\User; // Import User model
+use App\Models\User; 
 use Illuminate\Http\Request;
 
 class ScoreController extends Controller
